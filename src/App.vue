38a33e7bf33f145443b7978f7b3106a43a3b8b21
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2023-02-10 22:02:49 +0800

    Vue Init Template

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <div class="wrapper">
+    <header>
+    <h1>Blog Application</h1>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+  .wrapper {
+    flex: 1 0 auto;
+    max-width: 1280px;
+    width:100%;
+    margin: 0 auto;
+    background-color: white;
+  }
+</style>
