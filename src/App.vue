c685083a0a5ba8bc41f9aa84949bc8dcfe1fd7fa
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2023-02-11 19:53:34 +0800

    Added Footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
   import Header from "./components/Header.vue";
   import Home from "./components/Home.vue";
+  import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Header title="Leeman Tech Blog"/>
   <Home/>
+  <Footer/>
 </template>
 <style scoped>
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,8 +2,22 @@
 	
 </script>
 <template>
-	
+	<footer>
+		<img src="../assets/GitHub-Mark.png" alt="Github Logo">
+		<h2>Made By: Danvictorgithub</h2>
+	</footer>
 </template>
 <style scoped>
-	
+	footer {
+		height:100px;
+		padding:24px;
+		display:flex;
+		justify-content:center;
+		align-items:center;
+		border-top:2px solid lightgray;
+		gap:12px;
+	}
+	footer img {
+		height: 100%;
+	}
 </style>
