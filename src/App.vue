ec2dcc1cb3a24960769c40e8ee29dc9de4c65a57
Deviate-DV8 <88654163+Danvictorgithub@users.noreply.github.com>
2023-02-21 18:15:06 +0800

    added blogPost Component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
   import {RouterView} from 'vue-router';
-  import { ref, watch} from 'vue';
+  import {ref} from 'vue';
   import Header from "./components/Header.vue";
   import Footer from "./components/Footer.vue";
 </script>
--- src/views/Blogpost.vue
+++ src/views/Blogpost.vue
@@ -3,7 +3,14 @@
 </script>
 <template>
 	<section class="wrapper-blog container">
-		<h1>Hello World</h1>
+		<h1 class="blogTitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. </h1>
+		<img class="blogPostImg" src="../assets/blogPostImage.jpg">
+		<p class="blogContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est unde molestiae vitae odio tempora eos sapiente accusantium omnis, doloremque, ut similique obcaecati maxime odit neque consequatur provident nemo aliquam. Impedit! 
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Est unde molestiae vitae odio tempora eos sapiente accusantium omnis, doloremque, ut similique obcaecati maxime odit neque consequatur provident nemo aliquam. Impedit!
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Est unde molestiae vitae odio tempora eos sapiente accusantium omnis, doloremque, ut similique obcaecati maxime odit neque consequatur provident nemo aliquam. Impedit!
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Est unde molestiae vitae odio tempora eos sapiente accusantium omnis, doloremque, ut similique obcaecati maxime odit neque consequatur provident nemo aliquam. Impedit!
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Est unde molestiae vitae odio tempora eos sapiente accusantium omnis, doloremque, ut similique obcaecati maxime odit neque consequatur provident nemo aliquam. Impedit!
+</p>
 	</section>
 </template>
 <style scoped>
@@ -12,6 +19,21 @@
 	flex:1 0 auto;
 	max-width: 1280px;
 /*	background-color:lightgray;*/
+}
+.blogPostImg {
+	width:100%;
+	height: 425px;
+	object-fit:cover;
+}
+.blogTitle {
+	padding: 48px 36px;
+	font-size: 5rem;
+	font-weight: 600;
+	letter-spacing: 2px;
+}
+.blogContent {
+	padding: 60px 64px;
+	font-size:2rem;
 }
 @media (max-width:1280px){
 	.wrapper-blog {
