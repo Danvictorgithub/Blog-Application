38547e2bff2d6fd001846bc56979349408718238
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2024-03-26 23:31:25 +0800

    added changes to footer

--- src/components/ui/Footer.vue
+++ src/components/ui/Footer.vue
@@ -1,23 +1,21 @@
-<script setup>
-
-</script>
+<script setup></script>
 <template>
-	<footer>
-		<img src="../../assets/GitHub-Mark.png" alt="Github Logo">
-		<h2>Made By: Danvictorgithub</h2>
-	</footer>
+  <footer>
+    <!-- <img src="../../assets/GitHub-Mark.png" alt="Github Logo"> -->
+    <h2>Leeman's Tech Blog</h2>
+  </footer>
 </template>
 <style scoped>
-	footer {
-		height:100px;
-		padding:24px;
-		display:flex;
-		justify-content:center;
-		align-items:center;
-		border-top:1px solid lightgray;
-		gap:12px;
-	}
-	footer img {
-		height: 100%;
-	}
+footer {
+  height: 100px;
+  padding: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-top: 1px solid lightgray;
+  gap: 12px;
+}
+footer img {
+  height: 100%;
+}
 </style>
